f2030a9fcd0128da67d150cff3c4bb8c96771f05
ravraw <ravinrawat.wd@gmail.com>
2018-01-21 18:15:18 +0000

    response recieved as a data stream

--- https_example.js
+++ https_example.js
@@ -4,16 +4,18 @@ console.log("i did it !!!");
 
 var options = {
   host: "www.example.com",
-  path: "/"
+  path: "/",
 };
 
-var callback = function(){
-  console.log('In response handler callback!');
 
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
 
-https.request(options,callback).end();
+https.request(options, callback).end();
 
 console.log('I have nmade the request');
