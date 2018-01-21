50c65d9a89e0d02a2917638f9b2ae7ff53d22862
ravraw <ravinrawat.wd@gmail.com>
2018-01-21 18:04:14 +0000

    request made to exmple.com

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require("https");
 
 console.log("i did it !!!");
+
+var options = {
+  host: "www.example.com",
+  path: "/"
+};
+
+var callback = function(){
+  console.log('In response handler callback!');
+
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options,callback).end();
+
+console.log('I have nmade the request');
